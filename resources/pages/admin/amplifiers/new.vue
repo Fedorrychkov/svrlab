8027fe0e2cc3f097bb92c5f69a9738776bfc2be3
fedorrychkov <fedorrychkov@yandex.ru>
2019-03-03 12:49:15 +0300

    add new pages

---
+++ resources/pages/admin/amplifiers/new.vue
@@ -0,0 +1,32 @@
+<template>
+  <admin-template>
+    <template slot="title">
+      <h2 class="title">Новый товар</h2>
+    </template>
+    <template slot="content">
+      <pane>
+        <template slot="head">
+        </template>
+        <template slot="content">
+        </template>
+      </pane>
+    </template>
+  </admin-template>
+</template>
+<script>
+import AdminTemplate from '@/components/shared/AdminPageTemplate';
+import Pane from '@/components/shared/Pane';
+
+export default {
+  layout: 'admin',
+  components: {
+    AdminTemplate,
+    Pane
+  },
+  // head() {
+  //   return {
+  //     title: `[SVRLAB-ADMIN] - `
+  //   }
+  // }
+}
+</script>
---
+++ resources/pages/admin/orders/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <admin-template>
+    <template slot="title">
+      <h2 class="title">Заказы</h2>
+    </template>
+    <template slot="content">
+      <pane>
+        <template slot="head">
+        </template>
+        <template slot="content">
+        </template>
+      </pane>
+    </template>
+  </admin-template>
+</template>
+<script>
+import AdminTemplate from '@/components/shared/AdminPageTemplate';
+import Pane from '@/components/shared/Pane';
+
+export default {
+  layout: 'admin',
+  components: {
+    AdminTemplate,
+    Pane
+  }
+}
+</script>
