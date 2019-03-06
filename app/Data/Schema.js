6a3dedcd1e82442dec83c393c388c0509963a49f
fedorrychkov <fedorrychkov@yandex.ru>
2019-03-07 02:06:17 +0300

    update graphql schema

--- app/Data/Schema.js
+++ app/Data/Schema.js
@@ -1,18 +1,21 @@
 const { makeExecutableSchema } = require('graphql-tools');
 const { bookResolvers, typeBook } = require('./schema/books');
 const { postResolvers, typePost } = require('./schema/posts');
+const { amplifiertResolvers, typeAmplifier } = require('./schema/amplifiers');
 
 
 const typeDefs = `
-  type Query { posts: [Post], books: [Book] }
+  type Query { posts: [Post], books: [Book], amplifiers: [Amplifier] }
   ${typePost}
   ${typeBook}
+  ${typeAmplifier}
 `;
 
 const resolvers = {
   Query: {
     ...postResolvers.Query,
-    ...bookResolvers.Query
+    ...bookResolvers.Query,
+    ...amplifiertResolvers.Query
   }
 };
 
