7fda73ca58bca382840479df9c71e6e691e2a0ce
fedorrychkov <fedorrychkov@yandex.ru>
2019-03-07 22:05:01 +0300

    add update and get amplifier

--- resources/store/modules/amplifier.js
+++ resources/store/modules/amplifier.js
@@ -32,14 +32,16 @@ const state = () => ({
 });
 
 const getters = {
-  amplifiers: (state, getters, rootState) => {
+  amplifiers: (state, getters) => {
     return state.amplifiers;
+  },
+  amplifier: (state, getters) => {
+    return state.amplifier;
   },
 };
 
 const actions = {
   [ADD_AMPLIFIER]: ({commit, dispatch}, data) => {
-    data.customFields = JSON.stringify(data.customFields);
     const images = data.images.map(item => {
       return item.id;
     });
@@ -48,6 +50,23 @@ const actions = {
     commit(AMPLIFIER_ERROR, false);
     return new Promise((resolve, reject) => {
       axios.post('/api/amplifier', data).then(res => {
+        resolve(res);
+      }).catch(err => {
+        commit(AMPLIFIER_ERROR, true);
+      }).finally(() => {
+        commit(AMPLIFIER_LOADING, false);
+      });
+    });
+  },
+  [UPDATE_AMPLIFIER]: ({commit, dispatch}, data) => {
+    const images = data.images.map(item => {
+      return item.id;
+    });
+    data.images = JSON.stringify(images);
+    commit(AMPLIFIER_LOADING, true);
+    commit(AMPLIFIER_ERROR, false);
+    return new Promise((resolve, reject) => {
+      axios.put('/api/amplifier', data).then(res => {
         resolve(res);
       }).catch(err => {
         commit(AMPLIFIER_ERROR, true);
@@ -70,6 +89,20 @@ const actions = {
       });
     });
   },
+  [GET_AMPLIFIER]: ({commit, dispatch}, id) => {
+    commit(AMPLIFIER_LOADING, true);
+    return new Promise((resolve, rej) => {
+      axios.get(`/api/amplifier/${id}`).then(res => {
+        commit(GET_AMPLIFIER, res.data);
+        resolve(res);
+      }).catch(err => {
+        rej(err);
+        commit(AMPLIFIER_ERROR, true);
+      }).finally( () => {
+        commit(AMPLIFIER_LOADING, false);
+      });
+    });
+  },
   [PUSH_AMPLIFIER_IMAGES]: ({commit, dispatch}, data) => {
     return Promise((resolve, reject) => {
 
@@ -81,6 +114,14 @@ const mutations = {
   [ADD_AMPLIFIER]: (state, data) => {
     state.amplifiers.push(data);
   },
+  [UPDATE_AMPLIFIER]: (state, data) => {
+    const amplifiers = state.amplifiers.map(item => {
+      if (item.id === data.id) {
+        item = data;
+      }
+      return item;
+    });
+  },
   [AMPLIFIER_LOADING]: (state, status) => {
     state.loading = status;
   },
@@ -90,10 +131,13 @@ const mutations = {
   [GET_AMPLIFIERS]: (state, data) => {
     state.amplifiers = data.map(item => {
       item.customFields = JSON.parse(item.customFields);
-      item.images = JSON.parse(item.images);
       return item;
     });
-  }
+  },
+  [GET_AMPLIFIER]: (state, data) => {
+    data.customFields = JSON.parse(data.customFields);
+    state.amplifier = data;
+  },
 };
 
 export default {
