651ea272318138fecbd23a418f37dcdb6a07d025
fedorrychkov <fedorrychkov@yandex.ru>
2019-03-31 18:05:22 +0300

    add button with some icon

---
+++ resources/components/shared/ui/IconButton.vue
@@ -0,0 +1,36 @@
+<template>
+  <button class="icon-button" @click="$emit('click')">
+    <no-ssr>
+      <font-awesome-icon class="icon-button__icon" :icon="['fa', icon]" />
+    </no-ssr>
+    <span class="icon-button__text">{{text}}</span>
+  </button>
+</template>
+<script>
+export default {
+  props: ['icon', 'text'],
+}
+</script>
+<style lang="scss" scoped>
+.icon-button {
+  font-size: 18px;
+  color: var(--second-color);
+  border-radius: 4px;
+  border: 1px solid var(--second-color);
+  padding: 10px 15px;
+  transition: all .15s ease-out;
+
+  &:hover {
+    color: var(--primary-color);
+    background-color: var(--second-color);
+  }
+
+  &__icon {
+    margin-right: 8px;
+  }
+
+  &__text {
+    font-size: 18px;
+  }
+}
+</style>
