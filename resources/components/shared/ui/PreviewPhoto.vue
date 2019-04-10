27c5153a264d5406b832be65ea3ca06102ff6442
fedorrychkov <fedorrychkov@yandex.ru>
2019-04-11 00:25:11 +0300

    add component for product photo preview

---
+++ resources/components/shared/ui/PreviewPhoto.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="preview height">
+    <template v-for="img in item.images" >
+      <img :src="`/${img.path}/${img.large}`" :alt="`Картинка для ${item.name}`" :key="img.id" v-if="item.mainPhoto && item.mainPhoto === img.id" />
+    </template>
+    <img :src="`/${item.images[0].path}/${item.images[0].large}`" :alt="`Картинка для ${item.name}`" v-if="!item.mainPhoto" />
+  </div>
+</template>
+<script>
+export default {
+  props: ['item']
+}
+</script>
+<style lang="scss" scoped>
+.height {
+  height: 100%;
+}
+</style>
+
