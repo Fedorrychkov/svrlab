876af5d2ba084825f207b2a185434b03afab5287
fedorrychkov <fedorrychkov@yandex.ru>
2019-05-04 00:29:57 +0300

    add basket save

--- app/Controllers/Http/OrderController.js
+++ app/Controllers/Http/OrderController.js
@@ -31,6 +31,10 @@ class OrderController {
 
       const order = new Order()
 
+      if (!basket && !basket.length) {
+        response.badRequest('empty product list')
+      }
+
       if (customer.length) {
         order.customer_id = customer[0].id
       } else {
@@ -45,17 +49,30 @@ class OrderController {
         await customer.save()
         order.customer_id = customer.id
       }
+
       order.status_id = 1
       order.name = name
       order.zip = zip
       order.country = country
       order.city = city
       order.address = address
-      order.total = total
-      order.deliver = deliver
-      order.discount = discount
+      order.total = total.toFixed(2) || 0
+      order.deliver = deliver || 0
+      order.discount = discount || 0
       order.short = short
       await order.save()
+
+
+      await Promise.all(basket.map(async (item) => {
+        const product = new Basket()
+        product.order_id = order.id
+        product.product_id = item.id
+        product.quantity = item.quantity || 1
+        product.total = (item.quantity * item.cost).toFixed(2) || 0
+        product.cost = item.cost.toFixed(2) || 0
+        product.discount = item.discount || 0
+        await product.save()
+      }))
       response.ok({order: order, customer: customer})
     } catch (err) {
       response.badRequest(err)
