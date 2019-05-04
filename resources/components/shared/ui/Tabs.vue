f705da75be0003f4ea07b1eb5bb5d8435c07ea29
fedorrychkov <fedorrychkov@yandex.ru>
2019-05-05 01:56:25 +0300

    add orders tab

---
+++ resources/components/shared/ui/Tabs.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="tabs">
+    <ul class="tabs__head">
+      <li :class="{'tabs__tab': true, 'active': item.active}" v-for="item in items" :key="item.id" @click="$emit('clickedItem', item.id)">
+        <span>{{item.title}}</span>
+        <span class="value" v-if="item.orders.length"> {{item.orders.length}}</span>
+      </li>
+    </ul>
+    <div class="tabs__content">
+      <div :class="{'tabs__block': true}">
+        <slot name="content"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    items: Array
+  }
+}
+</script>
+<style lang="scss" scoped>
+.tabs {
+  margin: 30px 0;
+
+  &__head {
+    width: 100%;
+    display: flex;
+    justify-content: flex-start;
+    margin: 0 -8px;
+  }
+
+  &__tab {
+    padding: 8px;
+    border-radius: 4px 4px 0 0;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    cursor: pointer;
+
+    &.active {
+      background-color: rgba(0, 0, 0, .05);
+    }
+
+    .value {
+      margin-left: 2px;
+      font-size: 14px;
+      color: #999;
+    }
+  }
+
+  &__content {
+    background-color: rgba(0, 0, 0, .05);
+    padding: 8px;
+    margin: 0 -8px;
+    border-radius: 0 0 4px 4px;
+    min-height: 60px;
+  }
+}
+</style>
--- resources/pages/admin/orders/index.vue
+++ resources/pages/admin/orders/index.vue
@@ -8,9 +8,22 @@
         <template slot="head">
         </template>
         <template slot="content">
-          <div class="list">
-            <order-card v-for="item in orders" :key="item.id" :item="item" />
-          </div>
+          <!-- <template v-for="item in filtered">
+            <div class="list" :key="item.id">
+              <h5 class="title">{{item.title}}</h5>
+              <order-card v-for="order in item.orders" :key="order.id" :item="order" />
+            </div>
+          </template> -->
+          <tabs :items="filtered" @clickedItem="filterOrders">
+            <template slot="content">
+              <div class="list" v-if="filteredOrders">
+                <order-card v-for="order in filteredOrders" :key="order.id" :item="order" @updateStatus="updateStatus" :items="statuses" />
+              </div>
+              <div class="empty" v-if="!filteredOrders || !filteredOrders.length">
+                <p>Товаров с данным статусом - нет</p>
+              </div>
+            </template>
+          </tabs>
         </template>
       </pane>
     </template>
@@ -18,42 +31,104 @@
 </template>
 <script>
 import AdminTemplate from '@/components/shared/AdminPageTemplate';
+import Tabs from '@/components/shared/ui/Tabs';
 import Pane from '@/components/shared/Pane';
 import OrderCard from '@/components/core/orders/OrderCard';
-import { GET_ORDERS } from '@/store/actions/order.js';
+import {
+  GET_ORDERS,
+  GET_ORDER_STATUSES,
+  SET_STATUS
+} from '@/store/actions/order.js';
 
 export default {
   data() {
     return {
-      loading: true
+      loading: true,
+      filtered: null,
+      filteredOrders: null
     }
   },
   layout: 'admin',
   components: {
     AdminTemplate,
     Pane,
-    OrderCard
+    OrderCard,
+    Tabs
   },
   computed: {
     orders() {
       return this.$store.getters[`modules/order/orders`];
+    },
+    statuses() {
+      return this.$store.getters[`modules/order/statuses`];
+    }
+  },
+  watch: {
+    orders(items) {
+      this.filtered = this.statuses.map(item => {
+        item.active = false;
+        return item;
+      });
+      const filtered = this.filtered.map(item => {
+        item.orders = items.filter(order => item.id === order.status_id);
+        return item;
+      });
+      this.filtered = [
+        {id: 0, name: 'ALL', title: 'Все', active: false, orders: items},
+        ...filtered
+      ];
+      this.filterOrders(0);
     }
   },
   created() {
-    this.$store.dispatch(`modules/order/${GET_ORDERS}`)
-      .catch(err => {
+    this.$store.dispatch(`modules/order/${GET_ORDER_STATUSES}`)
+      .then(() => {
+        this.$store.dispatch(`modules/order/${GET_ORDERS}`)
+          .catch(err => {
+            console.log(err)
+          })
+      }).catch(err => {
+        console.log(err)
       }).finally(() => {
         this.loading = false;
       });
+  },
+  methods: {
+    filterOrders(val) {
+      this.filtered.forEach(item => {
+        item.active = false;
+        if (item.id === val) {
+          item.active = true;
+        }
+      })
+      this.filteredOrders = this.orders.filter(item => {
+        if (item.status_id === val) {
+          return item;
+        }
+        if (val === 0) {
+          return item;
+        }
+      });
+    },
+    updateStatus(data) {
+      this.$store.dispatch(`modules/order/${SET_STATUS}`, data);
+    }
   }
 }
 </script>
 <style lang="scss" scoped>
 .list {
-  margin: 15px -15px;
+  margin: 0 -15px;
+  padding: 8px 4px;
   display: flex;
-  justify-content: space-between;
+  justify-content: flex-start;
   flex-wrap: wrap;
+
+  .title {
+    width: 100%;
+    padding: 0 15px;
+    margin-bottom: 0;
+  }
 }
 </style>
 
