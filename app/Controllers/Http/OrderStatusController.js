76fdf1e0a173c24303c476767f694cc1ea3f2096
fedorrychkov <fedorrychkov@yandex.ru>
2019-05-05 01:54:58 +0300

    get status list

--- app/Controllers/Http/OrderStatusController.js
+++ app/Controllers/Http/OrderStatusController.js
@@ -1,6 +1,16 @@
 'use strict'
 
+const OrderStatus = use('App/Models/OrderStatus')
+
 class OrderStatusController {
+  async all ({response}) {
+    try {
+      const statuses = await OrderStatus.all()
+      response.ok(statuses)
+    } catch (err) {
+      response.badRequest(err)
+    }
+  }
 }
 
 module.exports = OrderStatusController
