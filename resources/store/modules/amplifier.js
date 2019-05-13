b22937ea29dfb8e4d4c76c4078f76a026155d403
fedorrychkov <fedorrychkov@yandex.ru>
2019-05-13 21:25:22 +0300

    remove semicolons

--- resources/store/modules/amplifier.js
+++ resources/store/modules/amplifier.js
@@ -8,9 +8,9 @@ import {
   AMPLIFIER_LOADING,
   AMPLIFIER_ERROR,
   AMPLIFIER_SUCCESS
-} from "../actions/amplifier";
-import gql from 'graphql-tag';
-import axios from 'axios';
+} from "../actions/amplifier"
+import gql from 'graphql-tag'
+import axios from 'axios'
 
 const amplifierMock = {
   id: 1,
@@ -22,133 +22,133 @@ const amplifierMock = {
   withdrawn: false, // true значит сняты с продажи
   inventory: 10, // Если инвентори пуст, то можно сделать под заказ
   images: null
-};
+}
 
 const state = () => ({
   loading: false,
   error: false,
   amplifiers: [],
   amplifier: []
-});
+})
 
 const getters = {
   amplifiers: (state, getters) => {
-    return state.amplifiers;
+    return state.amplifiers
   },
   amplifier: (state, getters) => {
-    return state.amplifier;
+    return state.amplifier
   },
-};
+}
 
 const actions = {
   [ADD_AMPLIFIER]: ({commit, dispatch}, data) => {
     const images = data.images.map(item => {
-      return item.id;
-    });
-    data.imageIDs = images;
-    data.type = 'AMPLIFIER';
-    data.images = JSON.stringify(images);
-    commit(AMPLIFIER_LOADING, true);
-    commit(AMPLIFIER_ERROR, false);
+      return item.id
+    })
+    data.imageIDs = images
+    data.type = 'AMPLIFIER'
+    data.images = JSON.stringify(images)
+    commit(AMPLIFIER_LOADING, true)
+    commit(AMPLIFIER_ERROR, false)
     return new Promise((resolve, reject) => {
       axios.post('/api/amplifier', data).then(res => {
-        resolve(res);
+        resolve(res)
       }).catch(err => {
-        commit(AMPLIFIER_ERROR, true);
+        commit(AMPLIFIER_ERROR, true)
       }).finally(() => {
-        commit(AMPLIFIER_LOADING, false);
-      });
-    });
+        commit(AMPLIFIER_LOADING, false)
+      })
+    })
   },
   [UPDATE_AMPLIFIER]: ({commit, dispatch}, data) => {
     const images = data.images.map(item => {
-      return item.id;
-    });
-    data.imageIDs = images;
-    data.type = 'AMPLIFIER';
-    data.images = JSON.stringify(images);
-    commit(AMPLIFIER_LOADING, true);
-    commit(AMPLIFIER_ERROR, false);
+      return item.id
+    })
+    data.imageIDs = images
+    data.type = 'AMPLIFIER'
+    data.images = JSON.stringify(images)
+    commit(AMPLIFIER_LOADING, true)
+    commit(AMPLIFIER_ERROR, false)
     return new Promise((resolve, reject) => {
       axios.put('/api/amplifier', data).then(res => {
-        resolve(res);
+        resolve(res)
       }).catch(err => {
-        commit(AMPLIFIER_ERROR, true);
+        commit(AMPLIFIER_ERROR, true)
       }).finally(() => {
-        commit(AMPLIFIER_LOADING, false);
-      });
-    });
+        commit(AMPLIFIER_LOADING, false)
+      })
+    })
   },
   [GET_AMPLIFIERS]: ({commit, dispatch}) => {
-    commit(AMPLIFIER_LOADING, true);
+    commit(AMPLIFIER_LOADING, true)
     return new Promise((resolve, rej) => {
       axios.get('/api/amplifier').then(res => {
-        commit(GET_AMPLIFIERS, res.data);
-        resolve(res);
+        commit(GET_AMPLIFIERS, res.data)
+        resolve(res)
       }).catch(err => {
-        rej(err);
-        commit(AMPLIFIER_ERROR, true);
+        rej(err)
+        commit(AMPLIFIER_ERROR, true)
       }).finally( () => {
-        commit(AMPLIFIER_LOADING, false);
-      });
-    });
+        commit(AMPLIFIER_LOADING, false)
+      })
+    })
   },
   [GET_AMPLIFIER]: ({commit, dispatch}, id) => {
-    commit(AMPLIFIER_LOADING, true);
+    commit(AMPLIFIER_LOADING, true)
     return new Promise((resolve, rej) => {
       axios.get(`/api/amplifier/${id}`).then(res => {
-        commit(GET_AMPLIFIER, res.data);
-        console.log(res);
-        resolve(res);
+        commit(GET_AMPLIFIER, res.data)
+        console.log(res)
+        resolve(res)
       }).catch(err => {
-        rej(err);
-        commit(AMPLIFIER_ERROR, true);
+        rej(err)
+        commit(AMPLIFIER_ERROR, true)
       }).finally( () => {
-        commit(AMPLIFIER_LOADING, false);
-      });
-    });
+        commit(AMPLIFIER_LOADING, false)
+      })
+    })
   },
   [PUSH_AMPLIFIER_IMAGES]: ({commit, dispatch}, data) => {
     return Promise((resolve, reject) => {
 
-    });
+    })
   }
-};
+}
 
 const mutations = {
   [ADD_AMPLIFIER]: (state, data) => {
-    state.amplifiers.push(data);
+    state.amplifiers.push(data)
   },
   [UPDATE_AMPLIFIER]: (state, data) => {
     const amplifiers = state.amplifiers.map(item => {
       if (item.id === data.id) {
-        item = data;
+        item = data
       }
-      return item;
-    });
+      return item
+    })
   },
   [AMPLIFIER_LOADING]: (state, status) => {
-    state.loading = status;
+    state.loading = status
   },
   [AMPLIFIER_ERROR]: (state, status) => {
-    state.error = status;
+    state.error = status
   },
   [GET_AMPLIFIERS]: (state, data) => {
     state.amplifiers = data.map(item => {
-      item.customFields = JSON.parse(item.customFields);
-      return item;
-    });
+      item.customFields = JSON.parse(item.customFields)
+      return item
+    })
   },
   [GET_AMPLIFIER]: (state, data) => {
-    data.customFields = JSON.parse(data.customFields);
-    state.amplifier = data;
+    data.customFields = JSON.parse(data.customFields)
+    state.amplifier = data
   },
-};
+}
 
 export default {
   state,
   getters,
   actions,
   mutations
-};
+}
 
